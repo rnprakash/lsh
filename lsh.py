942ff02624285283aeee2e0b142db79f3760f4cb
Rohith Prakash <rohithnprakash@gmail.com>
2017-04-07 13:33:32 -0500

    initial testing algo done

--- lsh.py
+++ lsh.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-import pandas
+import sys
 import numpy as np
 import random as rand
 from collections import defaultdict
@@ -13,22 +13,20 @@ class Hash:
         self.phi = phi
         self.d = d
 
-    
-    def train_behavior(self, t, R, K = [3], a = 5, M = 2**16):
+    def train(self, t, R, K = [3], a = 5, M = 2**16):
         self.R = R
         self.K = K
         self.a = a
         self.M = M
-        A = defaultdict(dict)
-        for i in enumerate(t):
+        self.A = defaultdict(dict)
+        for i in range(len(t)):
             for k in self.K:
                 if ((i * 2) % k) != 0:
                     continue
                 tk = t[-k:]
                 D = self.hash(tk)
                 for d,r in zip(D,self.R):
-                    A[r][d] = 1
-                return
+                    self.A[r][d] = 1
         return
 
     # t is a real-time time series
@@ -38,21 +36,21 @@ class Hash:
         behaviors = []
         for k in self.K:
             if ((i * 2) % k) != 0:
-                behaviors.append(None)
                 continue
             tk = t[-k:]
             # D, indexed by r \in R, is distance per units in R
             D = self.hash(tk)
             # Check for anomaly
-            behaviors.append( self.is_known(D) )
+            if self.is_anomalous(D):
+                behaviors.append((i,k))
         return behaviors
 
     def is_anomalous(self, D):
         # A[r] is dictionary of known values for threshold r. 0 is known, 1 is not
         # known is 2D list of behavior values
         known = [ 0 if d in self.A[r] else 1 for d,r in zip(D,self.R) ]
-        thresholds = [ sum( known[i:] ) for i in range(len(known[-1])) ]
-        return any( thresholds[i] >= i for i in range(len(thresholds)) )
+        thresholds = [ sum( known[i:] ) for i in range(len(known)) ]
+        return any( thresholds[i] > i for i in range(len(thresholds)) )
 
     # Hash time series with window length w
     # Window length is a variable parameter that defines the sensitivity of the hash
@@ -62,7 +60,7 @@ class Hash:
         # Project p onto $a$ random hyperplanes
         P = [ self.project(p, plane) for plane in self.make_planes(len(p)) ]
         # Get minimum distance from origin of points per unit r
-        D = [min( [self.d(0, p)/r for p in P] ) for r in R]
+        D = [ int(min( [self.d(np.zeros(len(p)), p)/r for p in P] )) for r in self.R]
         return D
 
     def project(self, p, plane):
@@ -81,12 +79,22 @@ class Hash:
             planes.append(b)
         return planes
 
-def f_deriv(ts):
-    return np.concatenate(([0], np.ediff1d(numpy.array(ts))))
+def phi(ts):
+    return np.concatenate((ts, np.ediff1d(np.array(ts))))
 
+def distance(x,y):
+    return np.linalg.norm(x-y)
 
 def main():
-    return
+    model = Hash(phi, distance)
+    with open(sys.argv[1]) as f:
+        t = map(float, f.read().split())
+    model.train(t, R=[25, 50, 100], a=5)
+    with open(sys.argv[2]) as f:
+        t = map(float, f.read().split())
+        pts = [m for m in [model.behavior_hash(t, i) for i in range(1, len(t))] if m]
+    print pts
+    print len(pts)*3/float(len(t))
 
 if __name__ == "__main__":
     main()
